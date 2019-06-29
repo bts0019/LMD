42eea059c3d12d64330cf25ae64654c468b85d08
bts0019 <15837005426@163.com>
2019-06-29 16:08:39 +0800

    userPower commit

--- Lmd_ServiceUserPower/src/main/java/com/sh/lmd/server/power/service/TCouponService.java
+++ Lmd_ServiceUserPower/src/main/java/com/sh/lmd/server/power/service/TCouponService.java
@@ -5,20 +5,15 @@ import com.sh.common.vo.R;
 public interface TCouponService {
     /**
      * 根据用户id 优惠卷状态和页数查询用户所属优惠卷
-     * @param uid
-     *  用户id
-     * @param status
-     * 优惠卷状态
-     * @param page
      * 页数
-     * @return
+     * @returnint uid, int status,int page
      */
-    R findCouponByuidAndstatus(int uid, int status,int page);
+    R findCouponByuidAndstatus(String token,int page);
 
     /**
      * 根据优惠卷查询优惠卷
      * @param password
      * @return
      */
-    R findCouponByPassword(String password,int uid);
+    R findCouponByPassword(String password,String token);
 }
--- Lmd_ServiceUserPower/src/main/java/com/sh/lmd/server/power/service/impl/TCouponServiceImpl.java
+++ Lmd_ServiceUserPower/src/main/java/com/sh/lmd/server/power/service/impl/TCouponServiceImpl.java
@@ -1,9 +1,13 @@
 package com.sh.lmd.server.power.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.sh.common.config.ProjectConfig;
+import com.sh.common.util.JedisUtil;
 import com.sh.common.vo.R;
 import com.sh.lmd.entity.TCoupon;
+import com.sh.lmd.entity.TUser;
 import com.sh.lmd.server.power.dao.TCouponMapper;
 import com.sh.lmd.server.power.service.TCouponService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +22,23 @@ public class TCouponServiceImpl implements TCouponService {
     @Autowired(required = false)
     private TCouponMapper couponDao;
 
+    @Autowired(required = false)
+    private JedisUtil jedisUtil;
+
     @Override
-    public R findCouponByuidAndstatus(int uid, int status,int page) {
+    public R findCouponByuidAndstatus(String token,int page) {
+        TUser user = JSON.parseObject(jedisUtil.get(ProjectConfig.TOKENJWT+token),TUser.class);
         Map<String,Integer> map = new HashMap<>();
-        map.put("uid",uid);
-        map.put("status",status);
+        map.put("uid",user.getUserid());
+        map.put("status",user.getStatus());
         PageHelper.startPage(page,6);
         PageInfo<TCoupon> pageInfo = new PageInfo<>(couponDao.selectCouponByUserAndStatus(map));
         return R.setOK("",pageInfo);
     }
 
     @Override
-    public R findCouponByPassword(String password,int uid) {
+    public R findCouponByPassword(String password,String token) {
+        TUser user = JSON.parseObject(jedisUtil.get(ProjectConfig.TOKENJWT+token),TUser.class);
         Map<String,Object> map = new HashMap<>();
         TCoupon tCoupon = couponDao.selectCouponByPassword(password);
         if (tCoupon == null){
@@ -39,7 +48,7 @@ public class TCouponServiceImpl implements TCouponService {
         }else {
 
             map.put("password",password);
-            map.put("uid",uid);
+            map.put("uid",user.getUserid());
             couponDao.updateCouponByPassword(map);
             return R.setOK("优惠卷兑换成功！",null);
         }
